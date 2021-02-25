b4b8a117dced6be23e749f95020e57f75d118749
TJ-coding <44460325+TJ-coding@users.noreply.github.com>
2021-02-25 08:40:35 +0000

    Code refactor

--- src/circle_detection.py
+++ src/circle_detection.py
@@ -20,14 +20,16 @@ def plot_circles(circles, bubble_id, color = (0, 0, 255)):
         # cv2.circle(frame, (x, y), radius, (0, 255, 0), 2)
         # draw the center of the circle
         cv2.circle(frame, (x, y), 1, color, 2)
-        cv2.putText(frame, str(id), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
+        cv2.putText(frame, str(id), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color)
 
 def get_circles(frame):
     blur_frame = cv2.GaussianBlur(frame, (11, 11), 50)
     gray_frame = cv2.cvtColor(blur_frame, 127, 255, cv2.COLOR_BGR2GRAY)
     # HoughCircles(Image, method, dp, minDist, param1: higher threshold of edge detection, param2: accumulator threshold )
     # 40
-    circles = cv2.HoughCircles(gray_frame, cv2.HOUGH_GRADIENT, 2, 150, param1=50, param2=18, minRadius=7, maxRadius=12)
+    dp = 2
+    circles = cv2.HoughCircles(gray_frame, cv2.HOUGH_GRADIENT, dp, int(24/dp), param1=40, param2=11,
+                               minRadius=int(14/dp), maxRadius=int(24/dp))
 
     if circles is not None:
         if len(circles) > 0:
@@ -72,33 +74,33 @@ def track_circles(tracking_circles, detecting_circles, min_cost = None):
         col_ind, row_ind = [], []
     return col_ind, row_ind
 
-def assign_ids_to_detected(detected_circles_len, previous_ids, col_indecie, row_indecies):
+def assign_ids_to_detected(detected_circles, previous_index2ids, current_index_of_bubble, previous_index_of_bubble):
     """
     col_indecies and row_indecies rovide index for mapping circles to previous frames to next frame
     next_ids[row_index] -> previous_ids[col_indecix]
     """
-    # initialize next_ids
-    next_ids = [-1 for i in range(detected_circles_len)]
-    paired_ids = set() # Used to age untracked ids
+    # initialize circle_index_to_id with -1
+    not_paired_circle_index = set([i for i in range(len(detected_circles))])
+    paired_circles = {}
+    paired_index = set()
     # Assign ids to new circles using previous circles ids [Inherit Id from previously detected objects]
-    for i in range(len(col_indecie)):
-        index_to_map_to = col_indecie[i]
-        index_to_map = row_indecies[i]
-        next_ids[index_to_map] = previous_ids[index_to_map_to]
-        paired_ids.add(previous_ids[index_to_map_to])
+    for i in range(len(current_index_of_bubble)):
+        index_to_map = current_index_of_bubble[i]
+        index_to_map_to = previous_index_of_bubble[i]
+        id = previous_index2ids[index_to_map_to]
+        circle = detected_circles[index_to_map]
+        paired_circles[id] = circle
+        not_paired_circle_index.remove(i)
     # Assign new ids to circles that are left over [Assigning ids to newly detected objects]
     # only called when detected_circles_len > tracked_circles_len
-    new_id = max(previous_ids) if len(previous_ids) !=0 else 0
+    new_id = max(previous_index2ids) if len(previous_index2ids) !=0 else 0
     # circles that were given new ids [not tracked from previously known bubbles]
-    newly_detected = {"ids": [], "index": set()} #index in detecting_circles that were given a brand new id [not trackted]
-    for i in range(len(next_ids)):
-        if next_ids[i] == -1:
-            next_ids[i] = new_id
-            newly_detected["ids"].append(new_id)
-            newly_detected["index"].add(i)
-            new_id += 1
+    newly_detected_circles = {}
+    for i in list(not_paired_circle_index):
+        newly_detected_circles[new_id] = detected_circles[i]
+        new_id += 1
 
-    return next_ids,  newly_detected, paired_ids
+    return newly_detected_circles, paired_circles
 
 def generate_training_data(frame,frame_id, circles, bubble_ids, image_size, file_path):
     # image_size must be odd
@@ -125,24 +127,13 @@ def generate_training_data(frame,frame_id, circles, bubble_ids, image_size, file
                 raise ValueError(f"{file_path}/{frame_id}-{id}.jpg")
             # next_file_name += 1
 
-def predict_tracking_circles(tracking_circles, tracking_kalman_filters):
-    for (key, value) in tracking_kalman_filters.items():
-        value.predict()
-        tracking_circles[key][0:2] = value.get_position()
-    return tracking_circles, tracking_kalman_filters
-
 def assign_tracking_circles(tracking_circles, tracking_kalman_filters, detecting_circles, bubble_ids, newly_detected):
-    """
-    for i in newly_detected_index:
-        id = bubble_ids[i]
-        tracking_circles[id] = detecting_circles[i]
-    """
     for i in range(len(detecting_circles)):
         id = bubble_ids[i]
         position_vector = [detecting_circles[i][0], detecting_circles[i][1]]
         if i in newly_detected["index"]:
             # Add newly detected circles as new circle
-            tracking_kalman_filters[id] = kf.KalmanFilter(position_vector, 0.01, 1, 0.0005)
+            tracking_kalman_filters[id] = kf.KalmanFilter(position_vector, 0.01, 0.01, 0.0005)
             tracking_circles[id] = detecting_circles[i]
         else:
             tracking_kalman_filters[id].update(position_vector)
@@ -150,72 +141,95 @@ def assign_tracking_circles(tracking_circles, tracking_kalman_filters, detecting
             tracking_circles[id][0:2] = [updated_position[0], updated_position[1]]
     return tracking_circles, tracking_kalman_filters
 
-def kill_old_tracked_circles(tracking_circles, tracking_kalman_filters, tracking_age, paired_ids, newly_detected, max_age = 7):
-    for id in newly_detected["ids"]:
-        tracking_age[id] = 0
-    for id in list(tracking_circles.keys()):
-        if id in paired_ids:
-            tracking_age[id] = 0
-        else:
-            tracking_age[id] += 1
-            if tracking_age[id] > max_age:
-                del tracking_circles[id]
-                del tracking_kalman_filters[id]
-                del tracking_age[id]
-    return tracking_circles, tracking_kalman_filters, tracking_age
+class TrackingPoints:
+    def __init__(self, max_age):
+        # Contains position, and radius of circles
+        self.circles = {}
+        self.kalman_filters = {}
+        self.age = {}
+        self.age_since_last_detected = {}
+        self.MAX_AGE = max_age
+
+    def assign_new_points(self, new_circles):
+        """
+        :param new_points:  {id, [x,y,radius]}
+        """
+        for (id, circle) in new_circles.items():
+            self.kalman_filters[id] = kf.KalmanFilter(circle[:2], 0.01, 1, 0.0005)
+            self.circles[id] = [self.kalman_filters[id].X[0, 0], self.kalman_filters[id].X[0, 1], new_circles[id][2]]
+            self.age[id] = 0
+            self.age_since_last_detected[id] = 0
+
+    def update_points(self, update_circles):
+        """
+        :param update_circles: {id, [x,y,radius]}
+        :return:
+        """
+        for (id, circle) in update_circles.items():
+            self.kalman_filters[id].update(circle[:2])
+            self.circles[id] = self.kalman_filters[id].get_position() + [circle[2]]
+            self.age_since_last_detected[id] = 0
+
+    def predict_points(self):
+        for id in self.kalman_filters:
+            self.kalman_filters[id].predict()
+            self.circles[id][:2] = self.kalman_filters[id].get_position()
+
+    def remove_old_points(self):
+        for id in list(self.circles):
+            self.age[id] += 1
+            self.age_since_last_detected[id] += 1
+            if self.age_since_last_detected[id] > self.MAX_AGE:
+                del self.circles[id]
+                del self.kalman_filters[id]
+                del self.age[id]
+                del self.age_since_last_detected[id]
+
 if __name__ == "__main__":
-    # a = track_circles([[1, 2], [3, 4], [5, 6]], [[1, 2], [3, 4], [5, 6]])
-    # print(a)
-    file_name = "Walker2.MOV"
+    file_name = "C1.mp4"
     vcap = cv2.VideoCapture(f"C:\\Users\\jinno\\OneDrive\\デスクトップ\\bscProject\\video_source\\{file_name}")
     TRAINING_DATA_PATH = "training_data/unlabeled"
     read_success, frame = vcap.read()
-    tracking_circles = None
-    tracking_kalman_filters = None
-    tracking_age = None
-    bubble_ids = []
+    tracking_points = None
+    bubble_index2ids = {}
     SHOW_EDGE_FRAME = True
     frame_id = 0
     while read_success:
         # Normalize image
         cv2.normalize(frame, frame, 0, 255, norm_type=cv2.NORM_MINMAX)
         detecting_circles = get_circles(frame)
-        if not tracking_circles is None and tracking_kalman_filters is not None and tracking_age is not None and not detecting_circles is None:
-            predict_tracking_circles(tracking_circles, tracking_kalman_filters)
+        if tracking_points is not None and detecting_circles is not None:
+            tracking_points.predict_points()
             # tracking_circles, tracking_kalman_filters = predict_tracking_circles(tracking_circles, tracking_kalman_filters)
-            cols, rows = track_circles(tracking_circles, detecting_circles, min_cost=10)
-            bubble_ids, newly_detected, paired_ids = assign_ids_to_detected(len(detecting_circles), list(tracking_circles.keys()), cols, rows)
+            previous_index_of_bubble, current_index_of_bubble = track_circles(tracking_points.circles, detecting_circles, min_cost=30)
+            newly_detected_circles, paired_circles = assign_ids_to_detected(detecting_circles, list(tracking_points.circles.keys()), current_index_of_bubble, previous_index_of_bubble)
             #generate_training_data(frame, frame_id, detecting_circles, bubble_ids, 65, TRAINING_DATA_PATH)
         else:
             # First iteration
-            bubble_ids, newly_detected, paired_ids = assign_ids_to_detected(len(detecting_circles), bubble_ids, [], [])
-            if tracking_circles is None:
+            newly_detected_circles, paired_circles = assign_ids_to_detected(detecting_circles, bubble_index2ids, [], [])
+            if tracking_points is None:
                 # Initialize tracking circles
-                tracking_circles = {}
-                tracking_kalman_filters = {}
-                tracking_age = {}
+                tracking_points = TrackingPoints(10)
 
-        tracking_circles, tracking_kalman_filters = assign_tracking_circles(tracking_circles, tracking_kalman_filters,
-                                                                            detecting_circles, bubble_ids, newly_detected)
-        tracking_circles, tracking_kalman_filters, tracking_age = kill_old_tracked_circles(tracking_circles,
-                                                                                           tracking_kalman_filters,
-                                                                                           tracking_age, paired_ids,
-                                                                                           newly_detected)
+        tracking_points.assign_new_points(newly_detected_circles)
+        tracking_points.update_points(paired_circles)
+        tracking_points.remove_old_points()
+
         print(f"time: {str(round(frame_id*1/30, 2))}" ) if frame_id % 30 == 0 else None
 
 
-        # Edge detection to see what hough circle is looking at for debugging
+        """# Edge detection to see what hough circle is looking at for debugging
         blur_frame = cv2.GaussianBlur(frame, (11, 11), 25)
         gray_frame = cv2.cvtColor(blur_frame, 127, 255, cv2.COLOR_BGR2GRAY)
         #20
-        edge_frame = cv2.Canny(gray_frame, 0, 50)
+        edge_frame = cv2.Canny(gray_frame, 0, 40)"""
 
         # plot_circles(detecting_circles, bubble_ids)
-        plot_circles(list(tracking_circles.values()), list(tracking_circles.keys()))
-        plot_circles(detecting_circles, list(bubble_ids), color=(255, 0, 0))
+        plot_circles(list(tracking_points.circles.values()), list(tracking_points.circles.keys()))
+        plot_circles(list(newly_detected_circles.values())+list(paired_circles.values()), list(newly_detected_circles.keys())+list(paired_circles.keys()), color=(255, 0, 0))
         cv2.putText(frame, "time: "+str(round(frame_id*1/30, 2)), (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
 
-        """cv2.imshow("test", frame)
+        """cv2.imshow("test", edge_frame)
         cv2.waitKey(0)"""
 
         # Save frames:
@@ -227,6 +241,7 @@ if __name__ == "__main__":
             video_writer = cv2.VideoWriter(f'video_log/{file_name.split(".")[0]}_result_{i}.avi',
                                            cv2.VideoWriter_fourcc(*"DIVX"),
                                            30, (frame.shape[1], frame.shape[0]))
+            print(f"{file_name.split('.')[0]}_result_{i}.avi")
         else:
             video_writer.write(frame)
         frame_id += 1
